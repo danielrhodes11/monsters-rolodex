dd361c05a429b7fee5abb74677590939aefffd00
Daniel Rhodes <danielrhodes47@gmail.com>
2024-08-19 16:35:54 -0500

    optomized code

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: ''
     };
+    console.log('constructor')
   }
 
   componentDidMount() {
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(() => {
@@ -23,11 +26,28 @@ class App extends Component {
         }))
   }
 
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }
+    })
+  }
+
   render() {
+    console.log('render')
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}
+        />
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return <div key={monster.id}><h1>{monster.name}</h1></div>
           })
         }
